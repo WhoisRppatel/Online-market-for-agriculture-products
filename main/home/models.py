90e0a914a8b4aae3b8ca49033411b27e11b2bac6
Rutvik Patel <patelrutvik.prakash007@gmail.com>
2019-01-24 19:49:20 +0530

    add product

--- main/home/models.py
+++ main/home/models.py
@@ -1,4 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Product(models.Model):
     name=models.CharField(max_length=50)
+    catagory=models.CharField(max_length=30,null=True)
+    description=models.CharField(max_length=100)
+    price=models.IntegerField()
+    #photo=models.ImageField
+    owner=models.OneToOneField(User,on_delete=models.CASCADE)
+    status=models.BooleanField(default=False)
+    qauntity=models.FloatField()
+    time=models.DateTimeField()
+
--- main/home/views.py
+++ main/home/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
+from django.shortcuts import render_to_response,redirect
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.template.context_processors import csrf
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.utils.timezone import timezone
 # Create your views here.
+def addproduct(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('addproduct.html',c)
+
+def newproduct(request):
+    name = request.POST.get('name', '')
+    catagory = request.POST.get('catagory', '')
+    date_time=timezone.now()
+    description = request.POST.get('description', '')
+    quantity = request.POST.get('quantity', '')
+    price = request.POST.get('price', '')
+
+    p = Product(name=name,catagory=catagory,time=date_time,description=description,price=price)
+    p.save()
+    return render_to_response('home.html')
